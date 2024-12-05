d54f8556bc8834ca090e7b2e218b2bc89d94b8c1
HappyOP411 <harpalkhachar9@gmail.com>
2024-12-05 15:09:11 +0530

    Add files via upload

---
+++ repitation logic/index.js
@@ -0,0 +1,59 @@
+// que1
+
+let tempreture = 30
+ 
+ if(tempreture<18){
+    console.log("It's too cold, turn on the heater.")
+  } else if(tempreture>18 && tempreture<25){
+    console.log("the temperature is comfortable")
+  } else{
+    console.log("It's too hot, turn on the fan.")
+  }
+
+//   que 2
+
+let score = 76
+if (score>90) {
+    console.log("you will get grade A")
+} else if(score>75) {
+    console.log("you will get grade B")
+} else if(score>50){
+    console.log("you will get grade C ")
+} else if (score<50){
+    console.log("you will get grade D")
+} 
+
+// que 3
+
+let light = "green";
+
+if (light === "green") {
+  console.log("Go");
+} else if (light === "orange") {
+  console.log("Slow down");
+} else if (light === "red") {
+  console.log("Stop");
+} else {
+  console.log("Unknown light color");
+}
+
+// que 4 
+
+let bill = 110
+
+if(bill < 50){
+    console.log("no discount")
+} else if (bill>50 && bill<100){
+    console.log("you wil get 10% discount")
+} else {
+    console.log("you will get 20% dicount")
+}
+
+// que 5
+// it is skipped becoz sir Told to skip this question
+
+
+// que 6 
+
+
+        
